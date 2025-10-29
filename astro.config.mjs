34c8794a3a81f0e1610d93da72979637b4bd62c0
Alex Xu <xualex008@gmail.com>
2025-10-29 09:48:10 -0400

    robot policy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import robotsTxt from 'astro-robots-txt'
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), 
+    robotsTxt({
+      policy: [
+        {
+          userAgent: '*',
+          disallow: '/'
+        }
+      ]
+    })
+  ]
 });
